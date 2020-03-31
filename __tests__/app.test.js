9ab702ccb128fc66e1243eaabf55cea5797cec4f
Michael Romay <amerror@gmail.com>
2020-03-30 17:01:06 -0700

    app sign up test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const User = require('../lib/models/User');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +17,18 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('can sign up a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({ email: 'wootie@birblife.com', password: 'tootie' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          email: 'wootie@birblife.com',
+          __v: 0
+        });
+      });
   });
 });
